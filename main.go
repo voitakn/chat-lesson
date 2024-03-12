efebd180495a30420cde0bb489da5d3b6d35f678
Constantin Voita <voitajw@gmail.com>
2024-03-12 19:42:42 +0000

    Project files

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	port string = ":8989"
+)
+
+var dbUser *os.File
+var dbMessage *os.File
+
+func main() {
+	// Data for users
+	var err error
+	dbUser, err = os.OpenFile("./data/users.txt",
+		os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		log.Panic("Error open file users.txt: " + err.Error())
+		return
+	}
+	defer dbUser.Close()
+
+	// Data for messages
+	dbMessage, err = os.OpenFile("./data/messages.txt",
+		os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		log.Panic("Error open file messages.txt: " + err.Error())
+		return
+	}
+	defer dbMessage.Close()
+
+	readAllUsers()
+
+	go handleMessages()
+
+	http.HandleFunc("/", checkService)
+	http.HandleFunc("/api/sign-in", signIn)
+	http.HandleFunc("/ws/", handleConnections)
+
+	log.Printf("Server started on %s", port)
+
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Panic("Error starting server: " + err.Error())
+	}
+}
+
+func checkService(w http.ResponseWriter, r *http.Request) {
+	responseString(w, `{"success": true}`)
+}
+
+func responseString(w http.ResponseWriter, text string) {
+	responseJson(w, []byte(text))
+}
+
+func responseJson(w http.ResponseWriter, v []byte) {
+	w.Header().Set("Content-Type", "application/json;  charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(v); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`Error`))
+	}
+}
---
+++ user.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func signIn(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		responseString(w, `{"success": false, "msg": "POST method is required"}`)
+		return
+	}
+	var user User
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		responseString(w, fmt.Sprintf(`{"success": false,"msg": "%s"}`, err.Error()))
+		return
+	}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responseString(w, fmt.Sprintf(`{"success": false, "msg": "%s"}`, err.Error()))
+		return
+	}
+	if len(user.Name) == 0 {
+		responseString(w, `{"success": false, "msg": "Please enter your name"}`)
+		return
+	}
+
+	userKey := fmt.Sprintf(`%s_%v`, user.Name, time.Now())
+	hasher := sha1.New()
+	hasher.Write([]byte(userKey))
+	userToken := hex.EncodeToString(hasher.Sum(nil))
+
+	user.Token = userToken
+	user.Id = len(userData.Items) + 1
+
+	userData.Items = append(userData.Items, user)
+	last := &userData.Items[len(userData.Items)-1]
+	userData.IDx[user.Id] = last
+	userData.TKx[user.Token] = last
+
+	userJson, _ := json.Marshal(user)
+
+	// Write new user to data file
+	_, err = fmt.Fprintln(dbUser, string(userJson))
+	if err != nil {
+		responseString(w, fmt.Sprintf(`{"success": false, "msg": "%v"}`, err.Error()))
+		return
+	}
+	responseJson(w, userJson)
+}
+
+func readAllUsers() {
+	scanner := bufio.NewScanner(dbUser)
+	for scanner.Scan() {
+		var u User
+		if err := json.Unmarshal([]byte(scanner.Text()), &u); err == nil {
+			userData.Items = append(userData.Items, u)
+			row := &userData.Items[len(userData.Items)-1]
+			userData.IDx[row.Id] = row
+			userData.TKx[row.Token] = row
+		}
+	}
+	log.Println("readAllUsers", userData)
+}
